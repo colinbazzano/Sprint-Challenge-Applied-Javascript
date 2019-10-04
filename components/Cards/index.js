a1467dace35d1d1169d59bef05d2440c0b30dc62
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-10-04 13:33:35 -0400

    finished the cards portion of the sprint challenge

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,57 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const container = document.querySelector('.cards-container');
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+    const articles = response.data.articles;
+
+    for (var key in articles) {
+        const info = articles[key];
+
+        info.forEach(element => {
+            createCard(element);
+            // const newCard = createCard(response.data.articles);
+            // container.appendChild(newCard);
+            // // console.log(element);
+        });
+      }//this closes for    
+    })
+    .catch(error => {
+        console.log('The data was not returned', error);
+    });
+
+function createCard(attributes) {
+    const card = document.createElement('div');
+    const cardHeadline = document.createElement('div');
+    const cardAuthor = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const cardCredit = document.createElement('span');
+
+    //content - remember that img gets an src not text content
+    cardHeadline.textContent = attributes.headline;
+    img.src = attributes.authorPhoto;
+    cardCredit.textContent = `By ${attributes.authorName}`;
+
+    //classes
+    card.classList.add('card');
+    cardHeadline.classList.add('headline');
+    cardAuthor.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    //append
+    card.appendChild(cardHeadline);
+    card.appendChild(cardAuthor);
+    cardAuthor.appendChild(imgContainer);
+    imgContainer.appendChild(img);
+    cardAuthor.appendChild(cardCredit);
+
+    container.appendChild(card);
+    return card
+    //REMEMBER TO APPEND THE CHILD!!!!!! this haunted you on the project & on the sprint
+}
+
